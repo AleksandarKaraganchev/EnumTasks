9eb2368a347a807e105e7be11f98a696ea15cdd8
AleksandarKaraganchev <aleksandar.karaganchev546@edu1.pgzaimov.com>
2024-12-16 14:55:51 +0200

    Add project files.

---
+++ Task1/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    internal class Program
+    {
+        enum Season
+        {
+            Winter,
+            Spring,
+            Summer,
+            Autumn
+        }
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a month in a number from 1 to 12 to see, which season it is(Winter, Spring, Summer, Autumn:");
+            int month = int.Parse(Console.ReadLine());
+            CaluclateMonthToSeason(month);
+        }
+
+        private static void CaluclateMonthToSeason(int month)
+        {
+            Season season;
+            if (month >= 1 && month <= 12)
+            {
+                if (month == 12 || month == 1 || month == 2)
+                {
+                    season = Season.Winter;
+                    Console.WriteLine($"Month {month} -> {season}.");
+                }
+                if (month >= 3 && month <= 5)
+                {
+                    season = Season.Spring;
+                    Console.WriteLine($"Month {month} -> {season}.");
+                }
+
+                if (month >= 6 && month <= 8)
+                {
+                    season = Season.Summer;
+                    Console.WriteLine($"Month {month} -> {season}.");
+                }
+                if (month == 9 || month == 10 || month == 11)
+                {
+                    season = Season.Autumn;
+                    Console.WriteLine($"Month {month} -> {season}.");
+                }
+            }
+            else
+            {
+                throw new ArgumentException("Please enter a nubmer from 1 to 12!!!");
+            }
+        }
+    }
+}
